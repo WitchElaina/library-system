562c334f75bd376c63619e773331dd9df95ac6dc
WitchElaina <msz13399173387@gmail.com>
2022-05-06 17:43:51 +0800

    Add search func

--- book.py
+++ book.py
@@ -18,10 +18,10 @@ class Book(object):
         self.name = name
         self.author = author
         self.classify_id = classify_id
+        self.publishing = publishing
 
     def export_info(self):
         ret = {
-            'ID':self.ID,
             'name': self.name,
             'author': self.author,
             'classify_id': self.classify_id,
@@ -30,6 +30,26 @@ class Book(object):
         }
         return ret
 
+    def keyword_match(self, keyword):
+        """match keyword in book info"""
+        # return if match success in each item
+        # example:
+        #   [0,1,0]: name False, author True, publishing False
+        ret_truth_table = [False, False, False]
+
+        # find author
+        if self.name.find(keyword) != -1:
+            ret_truth_table[0] = True
+        if self.author.find(keyword) != -1:
+            ret_truth_table[1] = True
+        if self.publishing.find(keyword) != -1:
+            ret_truth_table[2] = True
+
+        return ret_truth_table
+
+
+
 if __name__ == '__main__':
     book = Book(1, 'test', 'authorname', 0, 'pub')
     print(book.export_info())
+    print(book.keyword_match('te'))
